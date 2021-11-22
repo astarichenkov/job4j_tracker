e99707f8c6a7229d482e8dfbad7b6852af5b5b04
Anton.Starichenkov <anton.starichenkov@gmail.com>
2021-11-22 23:34:55 +0300

    0. Конфигурация Hibernate [#6872]

--- src/main/java/ru/job4j/tracker/model/Item.java
+++ src/main/java/ru/job4j/tracker/model/Item.java
@@ -1,19 +1,26 @@
 package ru.job4j.tracker.model;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
+@Entity
+@Table(name = "items")
 public class Item {
 
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
 
     private LocalDateTime created = LocalDateTime.now();
+
+    public Item() {
+    }
 
     public Item(String name) {
         this.name = name;
